03892f728e07deb4fba52a6fe0c6eda4f8419ab4
Moustikitos <moustikitos@gmail.com>
2021-12-10 21:17:58 +0100

    added short memory filter added block endpoint

--- slp/msg.py
+++ slp/msg.py
@@ -1,7 +1,9 @@
 # -*- coding:utf-8 -*-
 
 import slp
+import json
 import queue
+import hashlib
 import threading
 import traceback
 
@@ -9,12 +11,17 @@ from slp import node, chain, dbapi
 from usrv import srv
 
 
+@srv.bind("/block", methods=["POST"])
+def listen_blockchain(**request):
+    if request["method"] == "POST":
+        chain.manage_block(**request)
+
+
 # listen requests to /message endpoint
 @srv.bind("/message", methods=["HEAD", "POST"])
 def manage_message(**request):
     if request["method"] == "POST":
-        Messenger.JOB.put(request)
-        return "request queued"
+        return {"queued": Messenger.put(request)}
 
 
 # listen requests to /peers endpoint
@@ -22,6 +29,31 @@ def manage_message(**request):
 def send_peers(**request):
     if request["method"] == "GET":
         return list(node.PEERS)
+
+
+class Memory(queue.Queue):
+
+    LOCK = threading.Lock()
+
+    def __contains__(self, item):
+        with Memory.LOCK:
+            return Memory.hash_item(item) in self.queue
+
+    def put(self, item, block=True, timeout=None):
+        item_h = Memory.hash_item(item)
+        with Memory.LOCK:
+            if item_h not in self.queue:
+                if self.full():
+                    self.get()
+                queue.Queue.put(self, item_h, block, timeout)
+                return True
+
+    @staticmethod
+    def hash_item(item):
+        return hashlib.md5(
+            json.dumps(item, sort_keys=True, separators=(",", ":"))
+            .encode("utf-8")
+        ).hexdigest()
 
 
 class Messenger(threading.Thread):
@@ -32,6 +64,7 @@ class Messenger(threading.Thread):
     JOB = queue.Queue()
     LOCK = threading.Lock()
     STOP = threading.Event()
+    MEM = Memory(slp.JSON.get("message memory size", 20))
 
     def __init__(self, *args, **kwargs):
         threading.Thread.__init__(self)
@@ -53,7 +86,7 @@ class Messenger(threading.Thread):
                 elif "input" in msg:
                     # get wallet address from header and put to contract
                     headers = request.get("headers", {})
-                    msg["input"]["wallet"] = headers.get("Wallet-Address")
+                    msg["input"]["wallet"] = headers["Wallet-Address"]
                     dbapi.manage_input(msg)
                 elif "confirm" in msg:
                     chain.manage_confirm(msg)
@@ -62,8 +95,14 @@ class Messenger(threading.Thread):
                 slp.LOG.error("%r\n%s" % (error, traceback.format_exc()))
                 Messenger.LOCK.release()
 
-    def send(self, message):
-        pass
+    @staticmethod
+    def put(request):
+        # try to memorize message
+        queued = Messenger.MEM.put(request.get("data", {}))
+        # memorized
+        if queued:
+            Messenger.JOB.put(request)
+        return queued
 
 
 def stop():
