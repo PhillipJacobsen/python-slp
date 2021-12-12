06072468693d6a2568cba6ee16ef13d755f49a34
Moustikitos <moustikitos@gmail.com>
2021-12-12 08:16:42 +0100

    block processing added webhook subscription added

--- slp/chain.py
+++ slp/chain.py
@@ -9,8 +9,16 @@ import hashlib
 import traceback
 import threading
 
-from slp import serde, dbapi
+from slp import serde, dbapi, slp1
 from usrv import req
+
+
+def get_token_id(slp_type, symbol, blockheight, txid):
+    """
+    Generate token id.
+    """
+    raw = "%s.%s.%s.%s" % (slp_type, symbol, blockheight, txid)
+    return hashlib.md5(raw.encode("utf-8")).hexdigest().Decode("utf-8")
 
 
 def subscribe():
@@ -31,6 +39,7 @@ def subscribe():
         data["key"] = dump_webhook_token(data.pop("token"))
         with open(webhook, "w") as f:
             json.dump(data, f)
+    slp.LOG.info("Subscribed to %s", slp.JSON["webhook peer"])
 
 
 def unsubscribe():
@@ -47,22 +56,23 @@ def unsubscribe():
         if resp.get("status", 300) < 300:
             os.remove(data["key"])
             os.remove(webhook)
+        slp.LOG.info("Unsubscribed from %s", slp.JSON["webhook peer"])
         return resp
 
 
 def dump_webhook_token(token):
-    authorization = token[:32]  # "fe944e318edb02b979d6bf0c87978b64"
-    verification = token[32:]   # "0c8e74e1cbfe36404386d33a5bbd8b66"
+    authorization = token[:32]
+    verification = token[32:]
     filename = os.path.join(
         os.path.dirname(__file__),
         hashlib.md5(authorization.encode("utf-8")).hexdigest() + ".key"
     )
     with open(filename, "wb") as out:
-        pickle.dump({
+        pickle.dump(
+            {
                 "verification": verification,
                 "hash": hashlib.sha256(token.encode("utf-8")).hexdigest()
-            },
-            out
+            }, out
         )
     return filename
 
@@ -85,9 +95,8 @@ def check_webhook_token(authorization):
 
 
 def get_block_transactions(blockId):
+    data, page, result = [None], 1, []
     peer = slp.JSON["api peer"]
-    data, page = [None], 1
-    result = []
     while len(data) > 0:
         data = req.GET.api.blocks(
             blockId, "transactions", page=page, peer=peer
@@ -97,16 +106,20 @@ def get_block_transactions(blockId):
     return result
 
 
-def get_current_height():
-    return True or False
-
-
-def is_valid_transaction(txId):
-    return True or False
-
-
 def is_valid_address(address):
     return True or False
+
+
+def read_vendorField(vendorField):
+    contract = False
+    try:
+        contract = json.loads(vendorField)
+    except Exception:
+        try:
+            contract = serde.unpack_slp(vendorField)
+        except Exception:
+            pass
+    return contract
 
 
 def manage_block(**request):
@@ -114,28 +127,45 @@ def manage_block(**request):
     headers = request.get("headers", {})
     if not check_webhook_token(headers.get("Authorization", "?")):
         return False
-    # get transactions from block
     block = request.get("data", {})
-    tx_list = get_block_transactions(block["id"])
+    slp.LOG.info("Genuine block header received:\n%s", block)
+    return parse_block(block)
 
-    for index, tx in zip(list(range(len(tx_list))), tx_list):
-        try:
-            contract = serde.unpack_slp(tx["vendorField"])
-        except Exception as error:
-            slp.LOG.error("%r\n%s" % (error, traceback.format_exc()))
-        else:
-            slp_type, contract = contract.items()
-            if contract["tp"] == "GENESIS":
-                contract.update(
-                    owner=tx["senderId"],
-                    id=dbapi.get_token_id(
-                        slp_type, contract["sy"], block["height"], tx["id"]
+
+def parse_block(block):
+    # get transactions from block
+    tx_list = get_block_transactions(block["id"])
+    loop = zip(list(range(len(tx_list))), tx_list)
+    # search for SLP vendor fields in transfer type transactions
+    for index, tx in [(i+1, t) for i, t in loop if t["type"] == 0]:
+        # try to read contract from vendor field
+        contract = read_vendorField(tx["vendorField"])
+        if contract:
+            slp.LOG.info("> SLP vendorField found:\n%s", contract)
+            try:
+                slp_type, fields = contract.items()
+                # compute token id for GENESIS contracts
+                if fields["tp"] == "GENESIS":
+                    fields.update(
+                        id=get_token_id(
+                            slp_type, fields["sy"], block["height"], tx["id"]
+                        )
                     )
+                # add wallets information and tx amount as cost
+                fields.update(
+                    emitter=tx["senderId"], receiver=tx["recipientId"],
+                    cost=int(tx["amount"])
                 )
-            if dbapi.add_reccord(
-                block["height"], index, tx["id"], slp_type, **contract
-            ):
-                Chainer.JOB.put(contract)
+                # dbapi.add_reccord returns False if reccord already in
+                # journal.
+                if dbapi.add_reccord(
+                    block["height"], index, tx["id"], slp_type, **fields
+                ):
+                    slp.LOG.info("Document %s added to journal", fields)
+                    Chainer.JOB.put([slp_type, fields])
+            except Exception as error:
+                slp.LOG.error("%r\n%s" % (error, traceback.format_exc()))
+    return True
 
 
 def manage_confirm(msg):
@@ -158,10 +188,9 @@ class Chainer(threading.Thread):
         # controled infinite loop
         while not Chainer.STOP.is_set():
             try:
-                contract = Chainer.JOB.get()
-                if "slp1" in contract:
-                    if contract["slp1"]["tp"] == "GENESIS":
-                        dbapi.add_slp1_contract(**contract)
+                slp_type, contract = Chainer.JOB.get()
+                # get slp.`slp_type` module to manage the contract
+                getattr(slp, slp_type).manage(contract)
             except Exception as error:
                 slp.LOG.error("%r\n%s" % (error, traceback.format_exc()))
                 Chainer.LOCK.release()
--- slp/dbapi.py
+++ slp/dbapi.py
@@ -1,18 +1,16 @@
 # -*- coding:utf-8 -*-
 
 import slp
-import hashlib
 import traceback
 
-from slp import node, chain
 from pymongo import MongoClient
 
 #
 # MONGO DB DEFINITION ---
-# --- databases ---
 db = MongoClient(slp.JSON.get("mongo url", None))[slp.JSON["database name"]]
-db.contracts.create_index("tokenId", unique=True)
+# --- databases ---
 db.journal.create_index([("height", 'text'), ("index", 'text')], unique=True)
+db.contracts.create_index("tokenId", unique=True)
 # --- states ---
 db.wallets.create_index(
     [("address", 'text'), ("tokenId", 'text')],
@@ -22,34 +20,10 @@ db.supply.create_index("tokenId", unique=True)
 # ---
 
 
-def get_token_id(slp_type, symbol, blockheight, txid):
-    """
-    Generate token id.
-    """
-    raw = "%s.%s.%s.%s" % (slp_type, symbol, blockheight, txid)
-    return hashlib.md5(raw.encode("utf-8")).hexdigest().Decode("utf-8")
-
-
-def get_token_supply(tokenId):
-    supply = db.supply.find_one({"tokenId": tokenId})
-    return supply or {"error": "no contract found for tokenId %s" % tokenId}
-
-
-def transfer_token(tokenId, address, qt: slp.Quantity):
-    wallet = db.wallets.find_one({"address": address, "tokenId": tokenId})
-    if wallet:
-        return db.wallets.update_one(wallet, {"$inc": {"balance": qt.q}})
-    else:
-        return db.wallets.insert_one({
-            "tokenId": tokenId, "address": address, "balance": qt.q
-        })
-
-
 #
 # MONGO DB ATOMIC ACTION ---
 
-# on block.applied
-def add_reccord(height, index, txid, slp_type, **kw):
+def add_reccord(height, index, txid, slp_type, emitter, receiver, cost, **kw):
     """
     Add a reccord in the journal.
 
@@ -66,137 +40,22 @@ def add_reccord(height, index, txid, slp_type, **kw):
     Raises:
         Exception if reccord already in journal.
     """
+    fields = dict(
+        [k, v] for k, v in kw.items()
+        if k in "tp,id,de,qt,sy,na,du,no,pa,mi,ch,dt"
+    )
+    if not slp.validate(**fields):
+        slp.LOG.error("field validation did not pass")
+        return False
     try:
         db.journal.insert_one(
             dict(
                 height=height, index=index, txid=txid,
-                slp_type=slp_type, appliable=None,
-                **dict(
-                    [k, v] for k, v in kw.items()
-                    if k in [
-                        "tp", "id", "de", "qt", "sy", "na", "du", "no", "pa",
-                        "mi", "ch", "dt", "wt", "pk"
-                    ]
-                )
+                slp_type=slp_type, emitter=emitter, receiver=receiver,
+                legit=None, cost=cost, **fields
             )
         )
     except Exception as error:
         slp.LOG.error("%r\n%s", error, traceback.format_exc())
         return False
     return True
-
-
-def add_slp1_contract(owner, id, na, sy, qt, de, no, du, pa=False, mi=False):
-    # if all checks pass create the contract document
-    assert 3 <= len(sy) <= 8
-    assert 3 <= len(na) <= 24
-    assert 0 <= de <= 8
-    assert len(no) <= 32
-    assert len(du) <= 32
-
-    contract = dict(
-        tokenId=id, owner=owner, slpType="slp1",
-        name=na, symbol=sy, notes=no, uri=du,
-        globalSupply=qt, decimals=de,
-        pausable=pa, mintable=mi
-    )
-
-    try:
-        quantity = slp.Quantity(qt, de=de)
-        db.contrats.insert_one(contract)
-        db.supply.inserte_one(
-            {"tokenId": tokenId}, {
-                "tokenId": tokenId, "exchanged": 0, "minted": 0, "burned": 0,
-                "frozen": False
-            }
-        )
-        if not contract["mintable"]:
-            transfer_token(tokenId, owner, quantity)
-    except Exception as error:
-        slp.LOG.error("%r\n%s", error, traceback.format_exc())
-        return False
-    return True
-
-
-# def mint(height, timestamp, owner, tokenId, qt: slp.Quantity):
-#     contract = db.contracts.find({"tokenId": tokenId})
-#     if contract:
-#         transfer_token(tokenId, owner, qt.q)
-#         db.accountings.insert_one(
-#             dict(
-#                 height=height, timestamp=timestamp, tokenId=tokenId,
-#                 address=owner, minted=qt.q
-#             )
-#         )
-#     return {"error": "no contract found for tokenId %s" % tokenId}
-
-
-# # def swap(height, timestamp, tokenId, sender, recipient, qt: slp.Quantity):
-# #     quantity = qt.q
-# #     sender = db.wallets.find_one(address=sender, tokenId=tokenId)
-# #     if sender.get("balance", 0) >= quantity:
-# #         # update wallet state
-# #         db.wallets.update(sender, {'$inc': {"balance": -quantity}})
-# #         transfer_token(tokenId, recipient, qt)
-# #         # reccord transaction in accountings
-# #         return db.accountings.insert_many(
-# #             [
-# #                 dict(
-# #                     height=height, timestamp=timestamp, tokenId=tokenId,
-# #                     address=sender, exchanged=-quantity
-# #                 ),
-# #                 dict(
-# #                     height=height, timestamp=timestamp, tokenId=tokenId,
-# #                     address=recipient, exchanged=quantity
-# #                 )
-# #             ]
-# #         )
-# #     return False
-
-
-# # def burn(height, timestamp, owner, tokenId, qt: slp.Quantity):
-# #     return db.accountings.insert_one(
-# #         dict(
-# #             height=height, timestamp=timestamp, tokenId=tokenId,
-# #             address=owner, burned=qt.q
-# #         )
-# #     )
-
-
-# # def freeze(tokenId):
-# #     return db.supply.find_one_and_update(
-# #         {"tokenId": tokenId},
-# #         {"frozen", True}
-# #     )
-
-
-# # def unfreeze(tokenId):
-# #     return db.supply.find_one_and_update(
-# #         {"tokenId": tokenId},
-# #         {"frozen", False}
-# #     )
-
-
-# # def manage_input(msg):
-# #     if not slp.BLOCKCHAIN_NODE:
-# #         node.send_message({
-# #             "confirm": dict(
-# #                 msg["input"], **{"from": f"http://{slp.PUBLIC_IP}:{slp.PORT}"}
-# #             )
-# #         })
-# #     else:
-# #         # apply contract to database
-# #         pass
-
-
-# # def manage_confirm(msg):
-# #     pass
-
-
-# # def validate(contract):
-# #     supply = db.supply.find(tokenId=contract["tokenId"])
-# #     if supply.count() == 1:
-# #         if not supply["frozen"]:
-# #             #if is_legit(contract):
-# #                 return contract
-# #     return False
